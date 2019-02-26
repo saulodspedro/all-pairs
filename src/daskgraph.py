2de71a52d3775673feaf1b5e96d0e579b2706b9e
Saulo Pedro <saulods.pedro@gmail.com>
2019-02-26 18:13:54 -0300

    included dashboard

--- src/daskgraph.py
+++ src/daskgraph.py
@@ -1,8 +1,10 @@
 #dask graph
 
 import nltk
-from dask.multiprocessing import get
+from dask.distributed import Client
+from dask import delayed
 from textblob import TextBlob
+from os import cpu_count
 
 def load(filename):
     with open(filename, 'r') as myfile:
@@ -31,13 +33,25 @@ def build_pairs(data):
 
 def store(pairs):
     print(pairs)
-    
-dsk = {'load-1': (load, 'samples/acdc.txt'),
-       'load-2': (load, 'samples/queen.txt'),
-       'load-3': (load, 'samples/rolling.txt'),
-       'build_pairs-1': (build_pairs, 'load-1'),
-       'build_pairs-2': (build_pairs, 'load-2'),
-       'build_pairs-3': (build_pairs, 'load-3'),
-       'store': (store, ['build_pairs-%d' % i for i in [1,2,3]])}
 
-res = get(dsk, 'store')
+def main():
+
+    dsk = {'load-1': (load, '../samples/acdc_10'),
+           'load-2': (load, '../samples/queen_10'),
+           'load-3': (load, '../samples/rolling_10'),
+           'build_pairs-1': (build_pairs, 'load-1'),
+           'build_pairs-2': (build_pairs, 'load-2'),
+           'build_pairs-3': (build_pairs, 'load-3'),
+           'store': (store, ['build_pairs-%d' % i for i in [1,2,3]])}
+
+    client_opt = {'n_workers': cpu_count(),
+                  'memory_limit': 0,
+                  'processes': True,
+                  'diagnostics_port': 8787}
+
+    client = Client(**client_opt)
+
+    res = client.get(dsk, 'store')
+
+if __name__ == '__main__':
+    main()
